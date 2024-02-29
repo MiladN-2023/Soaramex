52b988ca128d9134bd582a26e879ba30fa46ee0c
MiladN-2023 <155392110+MiladN-2023@users.noreply.github.com>
2024-02-29 22:45:38 +0300

    Update layout.jsx

--- ‏‏my-app/src/app/layout.jsx
+++ ‏‏my-app/src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir='rtl'>
       <link rel="icon" href="../../public/icon.svg" />
-      <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7fLKQSP_UpOVDNYtgJOloK5w68a9oDqEURA&usqp=CAU" />
+      <meta property="og:image" content="https://www.logisticsmanager.com/wp-content/uploads/2022/09/Aramex-2.jpg" />
       <body className={tajawal.className}>
 
       {/* <NumTrack.Provider value={{NTrack,SetNtrack}}> */}
